d72f4f819fff212ab323c1f65f9d2508ee3b5693
Shlok Shah <106793433+shahshlok@users.noreply.github.com>
2025-12-05 22:01:07 -0800

    Deleting some stuff

--- authentic_seeded/a2/Rice_Joseph_548058/Q1.java
+++
@@ -1,1 +0,0 @@
-import java.util.Scanner;class Main{public static void main(String[]args){Scanner input=new Scanner(System.in);double v0=0,v1=0,t=0;double a=(v1-v0)/t;System.out.print("Enter v0, v1, and t: ");v0=input.nextDouble();v1=input.nextDouble();t=input.nextDouble();System.out.println("The average acceleration is "+a);}}
--- authentic_seeded/a3/Richardson_Eric_271506/Q1.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class Main{
-public static void main(String[]a){
-Scanner s=new Scanner(System.in);
-System.out.print("Enter v0, v1, and t: ");
-double v0=s.nextDouble(),v1=s.nextDouble(),t=s.nextDouble();
-System.out.println("The average acceleration is "+((v1-v0)/t));
-}
-}
